9dd71742f9ed02bbe379074587c7f811516e9e9b
Bryan Villafuerte <bryjho.villafuerte@yahoo.com.ph>
2020-11-08 23:39:50 +0100

    search filter work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Container, CardColumns, InputGroup, FormControl, Button } from 'react-bootstrap/';
+import React, { useState, useEffect } from 'react';
+import { Container, CardColumns, Jumbotron, Form } from 'react-bootstrap/';
 import TwitterStory from './components/TwitterStory';
 import data from './data/data.json';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+  
+  useEffect(() => {
+    const results = data.filter(outerData =>
+      outerData.documents.filter(innerData =>
+        innerData.author.displayName.includes(searchTerm.toLowerCase))
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <>
       <Container>
@@ -13,31 +28,28 @@ export default function App() {
           <h1 className='display-1'>Twitter Archive</h1>
         </Slide>
 
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="Input name of Author"
-            aria-label="Input name of Author"
-            aria-describedby="basic-addon2"
-          />
-          <InputGroup.Append>
-            <Button variant="outline-secondary">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-              </svg>
-              <span>Search</span>
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
+        <Jumbotron className="shadow">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Filter stories by name of author.</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name of author here"
+                value={searchTerm}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form>
+        </Jumbotron>
 
         <CardColumns>
-          {data.map((storyData) => 
+          {searchResults.map((outerData) => 
             <div>
-              {storyData.documents.map((moreStoryData) => 
-                <Fade bottom key={moreStoryData.id}>
-                  <TwitterStory storyTitle={moreStoryData.story.replace( /(<([^>]+)>)/ig, '')} author={moreStoryData.author.displayName} />
+              {outerData.documents.map((innerData) =>
+                <Fade bottom key={innerData.id}>
+                  <TwitterStory storyTitle={innerData.story.replace( /(<([^>]+)>)/ig, '')} author={innerData.author.displayName} />
                 </Fade>
-              )};
+              )}
             </div>
           )};
         </CardColumns>
---
+++ src/data/api.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://www.retriever-info.com/jorgen/tweets.json";
+export const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
